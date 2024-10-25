d1a489e9331a6cc107899c7941db0064f88f0151
Jashwanth <jashwanthreddy2021@gmail.com>
2024-10-25 19:05:00 +0530

    Latest changes

--- src/myjavaconcepts/MyChildToCallSuper.java
+++ src/myjavaconcepts/MyChildToCallSuper.java
@@ -5,6 +5,7 @@ public class MyChildToCallSuper extends MyParentSuper {
     MyChildToCallSuper(){
         super();
         System.out.println("India is the greatest country in the world");
+        System.out.println("New");
     }
 
     public static void main(String[] args) {
